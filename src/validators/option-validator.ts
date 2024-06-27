40c491fcfdb44e4cc1eb55b5c523626ce67c06ea
Roberto Bressanelli <rbressanelli@gmail.com>
2024-06-27 16:16:12 -0300

    Feat: back phone color customization options

---
+++ src/validators/option-validator.ts
@@ -0,0 +1,11 @@
+// bg-blue-950 border-blue-950
+// bg-zinc-900 border-zinc-900
+// bg-rose-600 border-rose-600
+// bg-yellow-500 border-yellow-500
+
+export const COLORS = [
+  { label: "Black", value: "black", tw: "zinc-900" },
+  { label: "Blue", value: "blue", tw: "blue-950" },
+  { label: "Rose", value: "rose", tw: "rose-600" },
+  { label: "Yellow", value: "yellow", tw: "yellow-500" },
+] as const;
